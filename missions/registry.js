4466f19222b5c55fcbff4fc70ee831a695ce0373
thomasjudes-del <thomas.judes@gmail.com>
2026-08-05 00:16:53 +0200

    Add mission registry

---
+++ missions/registry.js
@@ -0,0 +1,17 @@
+(() => {
+  'use strict';
+
+  window.IOTI_MISSION_REGISTRY = {
+    currentMissionId: '001',
+    missions: [
+      {
+        id: '001',
+        number: '001',
+        title: 'The Black Window',
+        status: 'current',
+        date: '2026-08-04',
+        config: 'missions/001.js'
+      }
+    ]
+  };
+})();
